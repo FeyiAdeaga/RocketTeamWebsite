0c13e00370c1afc6de211dec2cb99263cc9fd9f1
FeyiAdeaga <feyithereal@gmail.com>
2022-11-19 12:57:54 -0600

    cimmit 1 for contact

--- src/pages/contactpage/contact.jsx
+++ src/pages/contactpage/contact.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./contact.css";
 
-const Contactpage = () => <h1 className="homepage">CONTACT PAGE</h1>;
+const Contactpage = () =>
+    <h1 className="contactpage">
+        <div className="contact-title">
+            Contact US
+        </div>
+        <div className="emailus">
+            Email bleh
+        </div>
+    </h1>;
 
 export default Contactpage;
