cc87925a4e754ec85bb6165522cde6e9ea87e6a3
FeyiAdeaga <feyithereal@gmail.com>
2022-11-29 18:15:27 -0600

    home page styling

--- src/pages/homepage/home.jsx
+++ src/pages/homepage/home.jsx
@@ -3,7 +3,28 @@ import "./homepage.styles.scss";
 
 const Homepage = () => (
   <div className="homepage">
-    <h1>Home Page</h1>
+    <div className="home">
+      Home
+    </div>
+    <div className="learn-more">
+      <div className="learn-more-button">
+        LEARN MORE
+      </div>
+      <div className="welcome">
+      WELCOME TO THE OFFICIAL WEBSITE OF THE FISK ROCKET TEAM
+      </div>
+    </div>
+    <div className="subscribe">
+        <div className="get-new-content">
+        Get new content delivered directly to your inbox.
+        </div>
+        <div className="email-area">
+          <input type={"text"} placeholder= {"Email"} className={"input-field"}></input>
+          <div className="subscribe-button">
+            Subscribe
+          </div>
+        </div>
+      </div>
   </div>
 );
 
