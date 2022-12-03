c510c71a6e9e1dfd515a88e2614fdcb35f8a11d9
FeyiAdeaga <feyithereal@gmail.com>
2022-12-02 18:57:30 -0600

    Commi

--- src/pages/aboutpage/about.jsx
+++ src/pages/aboutpage/about.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "../aboutpage/about.css"
 
-const Aboutpage = () => <h1 className="homepage">ABOUT PAGE</h1>;
+const Aboutpage = () => (
+    <>
+    <div className="main">
+    <div className="about-us">
+        ABOUT US
+    </div>
+    <div className="rocket-pic">
+
+    </div>
+    <div className="where-it-began">
+       WHERE IT ALL BEGAN 
+    </div>
+    <div className="history">
+    For the past 5 years, a little know rocket program has emerged at Fisk University.  The program known as the Fisk Altitude Achievement Missile Team (F.A.A.M.T.)—or commonly known as Fisk Rocket Team—was created to compete in the NASA sponsored University Student Launch Initiative (USLI), but has grown into so much more.  The mandate of the competition was to design, build, and launch a rocket to take a scientific payload to an altitude of exactly one mile.  Schools were also expected to engage in outreach as part of their program.  When Fisk engaged in this outreach, it was quickly realized that there was a need for FAAMT’s involvement in outreach in Nashville’s Metro public schools.  In short, the outreach program took on a life of its own.
+    </div>
+    </div>
+    </>
+)
 
 export default Aboutpage;
